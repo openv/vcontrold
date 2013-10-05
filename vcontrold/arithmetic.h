f6e2d503ba8aace174085fe3f6a6d4a65dfb4e43
vheat <vheat@61131f00-6687-4361-811c-c0518eb8b609>
2013-10-05 17:58:16 +0000

    vcontrold und xml-32 Verzeichnis ist jetzt in trunk zu finden. Openv Wiki EInträge sind aktualisiert. Ein svn checkout alter Form zieht alles heraus einschleisslich der Branches. Das korrekte checkout Kommando ist jetzt:
    svn checkout svn://svn.code.sf.net/p/vcontrold/code/trunk/  vcontrold

--- vcontrold/arithmetic.h
+++
@@ -1,7 +0,0 @@
-/* arithemetic..h */
-/* Berechnung arithmetischer Ausdruecke */
-/* $Id: arithmetic.h 34 2008-04-06 19:39:29Z marcust $ */
-
-float execExpression(char **str,char *bPtr,float floatV, char *err); 
-int execIExpression(char **str,char *bPtr,char bitpos,char *pPtr,char *err); 
-
