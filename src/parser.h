389fbf5d8ebdd0b4b45a6a0dc7389e99a93043c9
Tobias Leupold <tobias.leupold@gmx.de>
2017-11-09 19:51:45 +0100

    Reworked parser.[c,h]. Are we really using goto here?!

--- src/parser.h
+++ src/parser.h
@@ -20,11 +20,12 @@
 int parseLine(char *lineo, char *hex, int *hexlen, char *uSPtr, ssize_t uSPtrLen);
 int execCmd(char *cmd, int fd, char *result, int resultLen);
 void removeCompileList(compilePtr ptr);
-int execByteCode(compilePtr cmpPtr, int fd, char *recvBuf, short recvLen, char *sendBuf, short sendLen, short supressUnit, char bitpos, int retry, char *pRecvPtr, unsigned short recvTimeout);
+int execByteCode(compilePtr cmpPtr, int fd, char *recvBuf, short recvLen, char *sendBuf,
+                 short sendLen, short supressUnit, char bitpos, int retry, char *pRecvPtr,
+                 unsigned short recvTimeout);
 void compileCommand(devicePtr dPtr, unitPtr uPtr);
 
-/* Token Definition */
-
+// Token Definition
 #define WAIT    1
 #define RECV    2
 #define SEND    3
