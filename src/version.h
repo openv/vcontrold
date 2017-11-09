d9661966d2de7e40d19771055f7b10f8ed5f1db8
Tobias Leupold <tobias.leupold@gmx.de>
2017-11-09 13:44:31 +0100

    Fixed the copyright header for version.h.

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-/*  Copyright 2007-2017 the original vcontrold development team
+/*  Copyright 2017 Sönke J. Peters <...>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
