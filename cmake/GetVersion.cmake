8b6f1fb37724b1a558f6322e29356fd8a9693aa7
Soenke J. Peters <peters__git@opcenter.de>
2017-11-09 11:24:18 +0100

    Removed left-overs from versioning scheme tests

--- cmake/GetVersion.cmake
+++ cmake/GetVersion.cmake
@@ -1,19 +1,3 @@
-# Get the current working branch
-execute_process(
-  COMMAND git rev-parse --abbrev-ref HEAD
-  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-  OUTPUT_VARIABLE GIT_BRANCH
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-# Get the latest abbreviated commit hash of the working branch
-execute_process(
-  COMMAND git log -1 --format=%h
-  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-  OUTPUT_VARIABLE GIT_COMMIT_HASH
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
 # Get version-string lookalike from git
 execute_process(
   COMMAND git describe --always --tags --dirty --long
@@ -26,15 +10,4 @@ set(VERSION "${GIT_CODE_VERSION}")
 message(STATUS "Version string: ${GIT_CODE_VERSION}")
 string(TIMESTAMP BUILD_TIMESTAMP %Y%m%d%H%M%S UTC)
 add_definitions(-DGIT_CODE_VERSION="${GIT_CODE_VERSION}" -DBUILD_TIMESTAMP="${BUILD_TIMESTAMP}")
-
-#parse the version information into pieces.
-string(REGEX REPLACE "^v([0-9]+)\\..*" "\\1" VERSION_MAJOR "${VERSION}")
-string(REGEX REPLACE "^v[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${VERSION}")
-string(REGEX REPLACE "^v[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_PATCH "${VERSION}")
-string(REGEX REPLACE "^v[.0-9]+-([0-9]+)-.*" "\\1" VERSION_BUILD "${VERSION}")
-string(REGEX REPLACE ".*-dirty$" "sdirty" VERSION_DIRTY "${VERSION}")
-set(VERSION_SHORT "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}-${VERSION_BUILD}")
-
-
-#configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cmake/version.h.in ${CMAKE_CURRENT_BINARY_DIR}/version.h)
 
