da4dee37dc226e49029325a9d77097af80bab5ca
Tobias Leupold <tobias.leupold@gmx.de>
2017-11-09 20:05:59 +0100

    Reworked socket.[c,h]. Are we really using goto here?!

--- src/socket.c
+++ src/socket.c
@@ -34,13 +34,13 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #ifndef __CYGWIN__
-/* I'm not sure about this cpp defines, can some check tht? -fn- */
+// I'm not sure about this cpp defines, can some check tht? -fn-
 #ifdef __linux__
-#include <linux/tcp.h>    /*do we realy need this? Not sure for Linux -fn- */
+#include <linux/tcp.h> // do we realy need this? Not sure for Linux -fn-
 #endif
 #if defined (__FreeBSD__) || defined(__APPLE__)
 #include <netinet/in.h>
-#include <netinet/tcp.h>    /* TCP_NODELAY is defined there -fn- */
+#include <netinet/tcp.h> // TCP_NODELAY is defined there -fn-
 #endif
 #endif
 
@@ -63,12 +63,14 @@ int openSocket(int tcpport)
     case 6:
         hints.ai_family = PF_INET6;
         break;
-    case 4:        /* this is for backward compatibility. We can explictly
-                     activate IPv6 with the -6 switch. Later we can use
-                     PF_UNSPEC as default and let the OS decide */
+    case 4:
+        // this is for backward compatibility. We can explictly
+        // activate IPv6 with the -6 switch. Later we can use
+        // PF_UNSPEC as default and let the OS decide
     default:
         hints.ai_family = PF_INET;
     }
+
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
     hints.ai_flags    = AI_PASSIVE;
@@ -80,8 +82,7 @@ int openSocket(int tcpport)
     free(port);
 
     if (n < 0) {
-        logIT(LOG_ERR, "getaddrinfo error:: [%s]\n",
-              gai_strerror(n));
+        logIT(LOG_ERR, "getaddrinfo error:: [%s]\n", gai_strerror(n));
         return -1;
     }
 
@@ -103,12 +104,10 @@ int openSocket(int tcpport)
             logIT1(LOG_ERR, "setsockopt gescheitert!");
             exit(1);
         }
-        if (!(listenfd < 0)) {
-            if (bind(listenfd,
-                     res->ai_addr,
-                     res->ai_addrlen) == 0)
-            { break; }
-
+        if (! (listenfd < 0)) {
+            if (bind(listenfd, res->ai_addr, res->ai_addrlen) == 0) {
+                break;
+            }
             close(listenfd);
             listenfd = -1;
         }
@@ -133,38 +132,36 @@ int openSocket(int tcpport)
 int listenToSocket(int listenfd, int makeChild, short (*checkP)(char *))
 {
     int connfd;
-    pid_t    childpid;
+    pid_t childpid;
     struct sockaddr_storage cliaddr;
-    socklen_t               cliaddrlen = sizeof(cliaddr);
+    socklen_t cliaddrlen = sizeof(cliaddr);
     char clienthost   [NI_MAXHOST];
     char clientservice[NI_MAXSERV];
 
     signal(SIGCHLD, SIG_IGN);
 
-    for ( ;;) {
-        connfd = accept(listenfd,
-                        (struct sockaddr *) &cliaddr,
-                        &cliaddrlen);
-
-        getnameinfo((struct sockaddr *) &cliaddr, cliaddrlen,
-                    clienthost, sizeof(clienthost),
-                    clientservice, sizeof(clientservice),
-                    NI_NUMERICHOST);
+    for (;;) {
+        connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &cliaddrlen);
+        getnameinfo((struct sockaddr *) &cliaddr, cliaddrlen, clienthost, sizeof(clienthost),
+                    clientservice, sizeof(clientservice), NI_NUMERICHOST);
 
         if (connfd < 0) {
             logIT(LOG_NOTICE, "accept auf host %s: port %s", clienthost, clientservice);
             close(connfd);
             continue;
         }
+
         logIT(LOG_NOTICE, "Client verbunden %s:%s (FD:%d)", clienthost, clientservice, connfd);
-        if (!makeChild) {
+        if (! makeChild) {
             return (connfd);
-        } else if ( (childpid = fork()) == 0) { /* unser Kind */
+        } else if ( (childpid = fork()) == 0) {
+            // unser Kind
             close(listenfd);
-            return (connfd);
+            return connfd;
         } else {
             logIT(LOG_INFO, "Child Prozess mit pid:%d gestartet", childpid);
         }
+
         close(connfd);
     }
 }
@@ -177,11 +174,9 @@ void closeSocket(int sockfd)
 
 int openCliSocket(char *host, int port, int noTCPdelay)
 {
-    struct addrinfo hints,                //< use hints for ipv46 address resolution
-               *res,
-               *ressave;
+    struct addrinfo hints, /* use hints for ipv46 address resolution */ *res, *ressave;
     int n, sockfd;
-    char port_string[16];            //< the IPv6 world use a char* instead of an int in getaddrinfo
+    char port_string[16]; // the IPv6 world use a char* instead of an int in getaddrinfo
 
     memset(&hints, 0, sizeof(struct addrinfo));
 
@@ -191,7 +186,6 @@ int openCliSocket(char *host, int port, int noTCPdelay)
 
     snprintf(port_string, sizeof(port_string), "%d", port);
     n = getaddrinfo(host, port_string, &hints, &res);
-
     if (n < 0) {
         logIT(LOG_ERR, "Fehler getaddrinfo: %s:%s", host, gai_strerror(n));
         exit(1);
@@ -201,13 +195,12 @@ int openCliSocket(char *host, int port, int noTCPdelay)
 
     sockfd = -1;
     while (res) {
-        sockfd = socket(res->ai_family,
-                        res->ai_socktype,
-                        res->ai_protocol);
-
-        if (!(sockfd < 0)) {
-            if (connect(sockfd, res->ai_addr, res->ai_addrlen) == 0)
-            { break; } // we have a succesfull connection
+        sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        if (! (sockfd < 0)) {
+            if (connect(sockfd, res->ai_addr, res->ai_addrlen) == 0) {
+                break;
+            }
+            // we have a succesfull connection
             close(sockfd);
             sockfd = -1;
         }
@@ -229,117 +222,126 @@ int openCliSocket(char *host, int port, int noTCPdelay)
     return sockfd;
 }
 
-/* Stuff aus Unix Network Programming Vol 1*/
+// Stuff aus Unix Network Programming Vol 1
+// include writen
 
-/* include writen */
-
-ssize_t                        /* Write "n" bytes to a descriptor. */
-writen(int fd, const void *vptr, size_t n)
+// Write "n" bytes to a descriptor.
+ssize_t writen(int fd, const void *vptr, size_t n)
 {
-    size_t        nleft;
-    ssize_t        nwritten;
-    const char    *ptr;
+    size_t nleft;
+    ssize_t nwritten;
+    const char *ptr;
 
     ptr = vptr;
     nleft = n;
     while (nleft > 0) {
-        if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
-            if (errno == EINTR)
-            { nwritten = 0; }        /* and call write() again */
-            else
-            { return (-1); }           /* error */
+        if ((nwritten = write(fd, ptr, nleft)) <= 0) {
+            if (errno == EINTR) {
+                nwritten = 0;
+                // and call write() again
+            }  else {
+                return -1;
+                // error
+            }
         }
-
         nleft -= nwritten;
         ptr   += nwritten;
     }
-    return (n);
+    return n;
 }
-/* end writen */
 
-ssize_t
-Writen(int fd, void *ptr, size_t nbytes)
+// end writen
+
+ssize_t Writen(int fd, void *ptr, size_t nbytes)
 {
     if (writen(fd, ptr, nbytes) != nbytes) {
         logIT1(LOG_ERR, "Fehler beim schreiben auf socket");
-        return (0);
+        return 0;
     }
     return (nbytes);
 }
 
-/* include readn */
+// include readn
 
-ssize_t                        /* Read "n" bytes from a descriptor. */
-readn(int fd, void *vptr, size_t n)
+// Read "n" bytes from a descriptor.
+ssize_t readn(int fd, void *vptr, size_t n)
 {
-    size_t    nleft;
-    ssize_t    nread;
-    char    *ptr;
+    size_t nleft;
+    ssize_t nread;
+    char *ptr;
 
     ptr = vptr;
     nleft = n;
     while (nleft > 0) {
-        if ( (nread = read(fd, ptr, nleft)) < 0) {
-            if (errno == EINTR)
-            { nread = 0; }        /* and call read() again */
-            else
-            { return (-1); }
-        } else if (nread == 0)
-        { break; }                /* EOF */
+        if ((nread = read(fd, ptr, nleft)) < 0) {
+            if (errno == EINTR) {
+                nread = 0;
+                // and call read() again
+            }
+            else {
+                return -1;
+            }
+        } else if (nread == 0) {
+            break;
+            // EOF
+        }
 
 #ifdef __CYGWIN__
-        if (nread > nleft)                // This is a workaround for Cygwin.
-        { nleft = 0; }                  // Here cygwins read(fd,buff,count) is
-        else                            // reading more than count chars! this is bad!
-        { nleft -= nread; }
+        // This is a workaround for Cygwin.
+        // Here cygwins read(fd,buff,count) is reading more than count chars! this is bad!
+        if (nread > nleft) {
+            nleft = 0;
+        } else {
+            nleft -= nread;
+        }
 #else
         nleft -= nread;
 #endif
         ptr   += nread;
     }
-    return (n - nleft);       /* return >= 0 */
-}
-/* end readn */
 
-ssize_t
-Readn(int fd, void *ptr, size_t nbytes)
+    return (n - nleft); //return >= 0
+}
+
+// end readn
+
+ssize_t Readn(int fd, void *ptr, size_t nbytes)
 {
-    ssize_t        n;
-
-    if ( (n = readn(fd, ptr, nbytes)) < 0) {
+    ssize_t n;
+    if ((n = readn(fd, ptr, nbytes)) < 0) {
         logIT1(LOG_ERR, "Fehler beim lesen von socket");
-        return (0);
+        return 0;
     }
-    return (n);
+    return n;
 }
 
-/* include readline */
+// include readline
 
-static ssize_t
-my_read(int fd, char *ptr)
+static ssize_t my_read(int fd, char *ptr)
 {
     static ssize_t read_cnt = 0;
-    static char    *read_ptr;
-    static char    read_buf[MAXLINE];
+    static char *read_ptr;
+    static char read_buf[MAXLINE];
 
     if (read_cnt <= 0) {
 again:
-        if ( (read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0) {
-            if (errno == EINTR)
-            { goto again; }
-            return (-1);
-        } else if (read_cnt == 0)
-        { return (0); }
+        if ((read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0) {
+            if (errno == EINTR) {
+                goto again;
+            }
+            return -1;
+        } else if (read_cnt == 0) {
+            return (0);
+        }
         read_ptr = read_buf;
     }
 
     read_cnt--;
     *ptr = *read_ptr++;
-    return (1);
+    return 1;
 }
 
-ssize_t
-readline(int fd, void *vptr, size_t maxlen)
+ssize_t readline(int fd, void *vptr, size_t maxlen)
 {
     int        n;
     ssize_t rc;
@@ -349,30 +351,37 @@ readline(int fd, void *vptr, size_t maxlen)
     for (n = 1; n < maxlen; n++) {
         if ( (rc = my_read(fd, &c)) == 1) {
             *ptr++ = c;
-            if (c == '\n')
-            { break; }    /* newline is stored, like fgets() */
+            if (c == '\n') {
+                break;
+                // newline is stored, like fgets()
+            }
         } else if (rc == 0) {
-            if (n == 1)
-            { return (0); }   /* EOF, no data read */
-            else
-            { break; }        /* EOF, some data was read */
-        } else
-        { return (-1); }       /* error, errno set by read() */
+            if (n == 1) {
+                return 0;
+                // EOF, no data read
+            }
+            else {
+                break;
+                // EOF, some data was read
+            }
+        } else {
+            return -1;
+            // error, errno set by read()
+        }
     }
 
-    *ptr = 0;    /* null terminate like fgets() */
-    return (n);
+    *ptr = 0; // null terminate like fgets()
+    return n;
 }
-/* end readline */
 
-ssize_t
-Readline(int fd, void *ptr, size_t maxlen)
+// end readline
+
+ssize_t Readline(int fd, void *ptr, size_t maxlen)
 {
-    ssize_t        n;
-
-    if ( (n = readline(fd, ptr, maxlen)) < 0) {
+    ssize_t n;
+    if ((n = readline(fd, ptr, maxlen)) < 0) {
         logIT1(LOG_ERR, "Fehler beim lesen von socket");
-        return (0);
+        return 0;
     }
-    return (n);
+    return n;
 }
