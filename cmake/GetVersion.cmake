b9a3aad52bebca8b0a5d8afd07ec3e9c4020e040
Soenke J. Peters <peters__git@opcenter.de>
2017-11-09 11:04:03 +0100

    version string message in cmake

--- cmake/GetVersion.cmake
+++ cmake/GetVersion.cmake
@@ -23,6 +23,7 @@ execute_process(
 )
 
 set(VERSION "${GIT_CODE_VERSION}")
+message(STATUS "Version string: ${GIT_CODE_VERSION}")
 string(TIMESTAMP BUILD_TIMESTAMP %Y%m%d%H%M%S UTC)
 add_definitions(-DGIT_CODE_VERSION="${GIT_CODE_VERSION}" -DBUILD_TIMESTAMP="${BUILD_TIMESTAMP}")
 
