2aff9916cfa04415ea4ac67c08aeacd5ff45a412
Tobias Leupold <tl@l3u.de>
2021-12-14 13:47:06 +0100

    Don't generate version.h in src/, but in the build dir
    So that we really only work inside the build directory and don't have
    any generated files outside.

--- cmake/modules/UpdateVersion.cmake
+++ cmake/modules/UpdateVersion.cmake
@@ -6,17 +6,14 @@
 # This file is under the public domain and can be reused without restrictions.
 
 if (NOT DEFINED BASE_DIR)
-    message (FATAL_ERROR "UpdateVersion.cmake: BASE_DIR not set. Please supply base working directory!")
+    message (FATAL_ERROR "UpdateVersion.cmake: BASE_DIR not set. "
+                         "Please supply base working directory!")
 endif()
 
-function (createVersionH version)
-    # write version info to a temporary file
-    configure_file (${BASE_DIR}/src/version.h.in ${CMAKE_CURRENT_BINARY_DIR}/version.h~)
-    # update info if changed
-    execute_process (COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_BINARY_DIR}/version.h~ ${BASE_DIR}/src/version.h)
-    # make sure info doesn't get stale
-    file (REMOVE ${CMAKE_CURRENT_BINARY_DIR}/version.h~)
-endfunction(createVersionH)
+# Version header locations
+set(VERSION_H_IN        ${BASE_DIR}/src/version.h.in)
+set(VERSION_H_CURRENT   ${CMAKE_BINARY_DIR}/version.h.current)
+set(VERSION_H_GENERATED ${CMAKE_BINARY_DIR}/version.h)
 
 # git or tarball?
 if (EXISTS ${BASE_DIR}/.git)
@@ -31,22 +28,32 @@ if (EXISTS ${BASE_DIR}/.git)
     endif()
 
     message (STATUS "Updating version information to ${VERSION} ...")
-    createVersionH(${VERSION})
 
 else()
     # --> tarball
     if (NOT EXISTS ${BASE_DIR}/src/version.h)
         message (WARNING "The generated file \"version.h\" does not exist!")
-        message (WARNING "Either, something went wrong when releasing this tarball, or this is some GitHub snapshot.")
+        message (WARNING "Either, something went wrong when releasing this tarball, or this is "
+                         "some GitHub snapshot.")
         get_filename_component(BASE_DIR_L "${BASE_DIR}" NAME)
-        string(REGEX REPLACE "[^-]+-([0-9]+)\\.([0-9]+)\\.([0-9]+)(.*)" "\\1.\\2.\\3\\4" VERSION "${BASE_DIR_L}")
+        string(REGEX REPLACE "[^-]+-([0-9]+)\\.([0-9]+)\\.([0-9]+)(.*)" "\\1.\\2.\\3\\4"
+                             VERSION "${BASE_DIR_L}")
         if ("${VERSION}" STREQUAL "${BASE_DIR_L}")
-            message(WARNING "Could not derive version from name of project directory, set version as \"unknown\".")
+            message(WARNING "Could not derive version from name of project directory, set version "
+                            "as \"unknown\".")
             set (VERSION "unknown")
         else()
             message(WARNING "Version guessed from project directory name as \"${VERSION}\".")
         endif()
         message (WARNING "Generating a dummy version.h.")
-        createVersionH(${VERSION})
     endif()
+
 endif()
+
+# write version info to a temporary file
+configure_file(${VERSION_H_IN} ${VERSION_H_CURRENT})
+# update info if changed
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                        ${VERSION_H_CURRENT} ${VERSION_H_GENERATED})
+# make sure info doesn't get stale
+file(REMOVE ${VERSION_H_CURRENT})
